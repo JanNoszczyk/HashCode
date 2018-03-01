1c135bedcc1218ef592f07b9fd9d35e90dd30605
Bernd Prach <bprach@live.com>
2018-03-01 20:01:39 +0100

    changed cars

--- real_round/car.py
+++ real_round/car.py
@@ -13,41 +13,74 @@ def read(file_path):
             position_start_x, position_start_y, position_end_x, position_end_y, \
                 time_start, time_end = tuple(map(int, lines[i].split(' ')))
             rides.append(Ride((position_start_x, position_start_y), \
-                (position_end_x, position_end_y), time_start, time_end))
+                (position_end_x, position_end_y), time_start, time_end, i-1))
+            #rides.append(Ride((position_start_x, position_start_y), \
+            #    (position_end_x, position_end_y), time_start, time_end))
     return int(rows), int(columns), int(vehicles), int(number_rides), int(bonuses), \
               int(steps), rides
 
-
+def output_cars(cars, file_path):
+    with open(file_path, "w") as f:
+        for car in cars:
+            ids = []
+            for ride in cars.rides:
+                ids.append(ride.id)
+                final_list = [len(ids)] + ids
+                string = " ".join(final_list)
+                f.write(string+"\n")         
+              
 def distance(start_x, start_y, end_x, end_y):
     return abs(start_x-end_x) + abs(start_y-end_y)
 
+def distance(start_position, end_position):
+    return abs(start_position[0]-end_position[0]) + \
+        abs(start_position[1]-end_position[1])
+
 class Ride:
+    id = ""
     position_start = []
     position_end = []
     time_start = ""
     time_end = ""
 
-    def __init__(self, position_start, position_end, time_start, time_end):
+    def __init__(self, position_start, position_end, time_start, time_end, id):
         self.position_start = position_start
         self.position_end = position_end
+        self.id = id
 
     def distance(self):
         return abs(self.position_start[0]-self.position_start[0]) + abs(self.position_start[1]-self.position_end[1])
 
     def in_time(self, start_time):
         return start_time - self.distance() < self.time_end
+        
+    def finish_at(self, car_start_time, car_position)
+        arrive_time = car_start_time + distance(car_position,self.position_start)
+        ride_start_time = max(arrive_time,self.time_start)
+        ride_end_time = ride_start_time + distance(position_start,position_end)
+        return ride_end_time
 
+   
+        
+        
 class Car:
     rides = []
     start_time = []
     done = []
+    is_busy = False
+    busy_till = 0
+    position = [0,0]
+    
     def __init__(self):
         self.rides = []
 
     def add_journey(self, start_time, ride):
         if ride.in_time(start_time) and not Car.is_busy():
-            self.journey.append(ride)
+            self.rides.append(ride)
+            self.busy_till = ride.finish_at(busy_till,rides[-1].position_end)
 
+            
+            
     def is_busy(self, start_time, end_time):
         for ride in self.rides:
             if (start_time > ride.time_start \
